72fdc3ad8aeece73a9abd32389db4b8733958e90
idragonb <git@ilact.com>
2012-07-29 11:54:56 +0300

    First revision

--- htmlhead.php
+++ htmlhead.php
@@ -2,7 +2,7 @@
 <meta content="text/html; charset=UTF-8" http-equiv="content-type">
 <title>E.M.Tech</title>
 
-<script type="text/javascript" src="xmlFunctions27.js"></script>
+<script type="text/javascript" src="xmlFunctions28.js"></script>
 <link rel="stylesheet" type="text/css" href="emtech39.css" />
 
 </head>
---
+++ services.php
@@ -0,0 +1,27 @@
+﻿
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+
+<html>
+<?php include("htmlhead.php"); ?>
+<body>
+
+<br />
+
+<div id="container" class="centered">
+<?php include("menu.php"); ?>
+    <iframe id="servicescontent" src="ServicesGeneralEnglish.html" width="100%" height="2500" scrolling="auto" frameborder="0">
+	</iframe>
+	<p id="about">--</p>
+</div>
+
+<script type="text/javascript"> // Places title upon load in paragraph with id "title"
+    lang = document.getElementById("languageButton").value;
+	document.getElementById("languageButton").value="עברית";	
+	
+	putlanguage(lang);
+	
+
+
+</script>
+
+</body></html>
